e346175fbdfdc663901bf834a2108a0ca45e050b
alejandrohsanchez <asanchez9112@gmail.com>
2022-03-09 01:19:23 -0800

    Created project
    Uses SQLite database to save and print data onto app.

---
+++ SQLDemo/app/src/main/java/com/example/sqldemo/DatabaseHelper.java
@@ -0,0 +1,107 @@
+package com.example.sqldemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Type in extends SQLiteOpenHelper and the rest should fill in
+// Go over the error and implement methods
+// There will be no default constructor available
+// This is a problem because we're extending from a parent class
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String customers = "customers";
+
+    // Public constructor found from extends error, or going to SQLOpenHelper site
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "customer.db", null, 1);
+    }
+
+    // this is called the first time a database is accessed. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableStatement =   "CREATE TABLE customers (c_id INT, c_name TEXT, c_age INT, c_active BOOL);";
+        sqLiteDatabase.execSQL(createTableStatement);
+    }
+
+    // this is called if the database version number changes.
+    // It prevents previous users apps from breaking when you change the database design.
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    public boolean addOne(CustomerModel customerModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Acts like a hashmap
+        ContentValues cv = new ContentValues();
+
+        cv.put("c_name", customerModel.getName());
+        cv.put("c_age", customerModel.getAge());
+        cv.put("c_active", customerModel.isActive());
+
+        if (cv.get("c_name") == "error" || cv.get("c_name").toString().isEmpty()) {
+            return false;
+        }
+        else {
+            long insert = db.insert("customers", null, cv);
+            if (insert == -1) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    public List<CustomerModel> getEveryone() {
+        List<CustomerModel> returnList = new ArrayList<>();
+
+        // get data from the database
+        String queryString = "SELECT * FROM customers";
+        /* Whenever you open up a writable database, the database becomes locked,
+        * so no other operations can be done.
+        */
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Selecting rawQuery that returns a cursor.
+        // Introduced local variable. Cursor is result set.
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        // Move to the first result in the result set.
+        if (cursor.moveToFirst()) {
+            // loop through the cursor (result set) and create new customer objects.
+            // Put them into the return list.
+            do {
+                int customerID = cursor.getInt(0);
+                String customerName = cursor.getString(1);
+                int customerAge = cursor.getInt(2);
+                boolean customerActive = cursor.getInt(3) == 1 ? true:false;
+
+                CustomerModel newCustomer = new CustomerModel(customerID, customerName, customerAge, customerActive);
+                returnList.add(newCustomer);
+
+
+            } while (cursor.moveToNext());
+        }
+        else {
+            /*
+            * Failure. Do not add anything to the list.
+            * */
+        }
+
+        // Close both the cursor and the db when done.
+        cursor.close();
+        db.close();
+
+        return returnList;
+    }
+
+}
--- SQLDemo/app/src/main/java/com/example/sqldemo/MainActivity.java
+++ SQLDemo/app/src/main/java/com/example/sqldemo/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // references to buttons and other controls that are on the layout
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     EditText et_name, et_age;
     Switch sw_activeCustomer;
     ListView lv_customerList;
+    DatabaseHelper databaseHelper;
+    ArrayAdapter customerArrayAdapter;
 
 
 
@@ -35,25 +40,55 @@ public class MainActivity extends AppCompatActivity {
         sw_activeCustomer = findViewById(R.id.sw_active);
         lv_customerList = findViewById(R.id.lv_customerList);
 
-        // button listeners for the add and view all buttons
+        // Make new database helper object.
+        databaseHelper = new DatabaseHelper(MainActivity.this);
 
+        // MainActivity.this is our context.
+        // Associate this array adapter with the control on the screen.
+        ShowCustomerOnListView();
+
+        // button listeners for the add and view all buttons
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //
-                CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
 
-                Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                CustomerModel customerModel;
+
+                try {
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error creating user", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, false);
+
+
+                }
+
+                // Makes a reference to the new customer database
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+                boolean success = databaseHelper.addOne(customerModel);
+                Toast.makeText(MainActivity.this, "Success=" + success, Toast.LENGTH_SHORT).show();
+
+                ShowCustomerOnListView();
             }
         });
 
         btn_viewAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "View All button", Toast.LENGTH_SHORT).show();
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+                ShowCustomerOnListView();
+                //Toast.makeText(MainActivity.this, everyone.toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
 
+    }
+
+    private void ShowCustomerOnListView() {
+        customerArrayAdapter = new ArrayAdapter<CustomerModel>(MainActivity.this, android.R.layout.simple_list_item_1, databaseHelper.getEveryone());
+        lv_customerList.setAdapter(customerArrayAdapter);
     }
 }
