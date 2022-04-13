d506bd17f11678d211c0fecfb5046f82e32379fa
cgarcia311 <90656904+cgarcia311@users.noreply.github.com>
2022-04-13 08:38:42 -0700

    weekly calendar

--- Cesar/workoutapplication/app/src/main/java/com/cesargarcia/workoutapplication/MainActivity.java
+++ Cesar/workoutapplication/app/src/main/java/com/cesargarcia/workoutapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cesargarcia.workoutapplication;
 
+import static com.cesargarcia.workoutapplication.CalendarUtils.daysInMonthArray;
+import static com.cesargarcia.workoutapplication.CalendarUtils.monthYearFromDate;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -19,14 +23,13 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
-    private LocalDate selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
-        selectedDate = LocalDate.now();
+        CalendarUtils.selectedDate = LocalDate.now();
         setMonthView();
     }
 
@@ -37,8 +40,8 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
     }
     private void setMonthView() {
-        monthYearText.setText(monthYearFromDate(selectedDate));
-        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
 
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
@@ -47,44 +50,29 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
     }
 
-    private ArrayList<String> daysInMonthArray(LocalDate date) {
-        ArrayList<String> daysInMonthArray = new ArrayList<>();
-        YearMonth yearMonth = YearMonth.from(date);
-        int daysInMonth = yearMonth.lengthOfMonth();
-        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
-        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
-        for(int i = 1; i<= 42; i++){
-            if(i <= dayOfWeek || i > daysInMonth + dayOfWeek){
-                daysInMonthArray.add("");
-            }
-            else{
-                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
-            }
-        }
-        return daysInMonthArray;
-    }
 
-    private String monthYearFromDate(LocalDate date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
-        return date.format(formatter);
-    }
 
     public void previousMonthAction(View view) {
-        selectedDate = selectedDate.minusMonths(1);
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
         setMonthView();
     }
 
     public void nextMonthAction(View view) {
-        selectedDate=selectedDate.plusMonths(1);
+        CalendarUtils.selectedDate=CalendarUtils.selectedDate.plusMonths(1);
         setMonthView();
     }
 
     @Override
-    public void onItemClick(int position, String dayText) {
-        if(dayText.equals("")){
-            String message = "Selected Date" + dayText + " " + monthYearFromDate(selectedDate);
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    public void onItemClick(int position, LocalDate date) {
+        if(date != null){
+        CalendarUtils.selectedDate= date;
+        setMonthView();
         }
 
+
+    }
+
+    public void weeklyAction(View view) {
+        startActivity(new Intent(this, WeekViewActivity.class));
     }
 }
